0dbaf5aa3ab38ae4b4b7795737328fcc0624b35d
Juan Antonio Barragan <barragan@purdue.edu>
2020-04-28 16:15:28 -0400

    transfer learning across user bug fix.

---
+++ PowerClassification/Utils/examples/01_TestMultiUserModel.py
@@ -0,0 +1,50 @@
+from PowerClassification.Utils.NetworkTraining import CrossValidationRoutines
+from PowerClassification.Utils.NetworkTraining import Utils
+from PowerClassification.Utils.NetworkTraining import TransferLearningModule
+from pathlib import Path
+import pandas as pd
+import itertools
+
+'''
+EEG_CHANNELS = [
+        "FP1", "FP2", "AF3", "AF4", "F7", "F3", "FZ", "F4",
+        "F8", "FC5", "FC1", "FC2", "FC6", "T7", "C3", "CZ",
+        "C4", "T8", "CP5", "CP1", "CP2", "CP6", "P7", "P3",
+        "PZ", "P4", "P8", "PO3", "PO4", "OZ"]
+POWER_COEFFICIENTS = ['Low', 'Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']
+'''
+
+if __name__ == '__main__':
+    USERS = ['ryan','juan', 'jackie','jhony']
+    EEG_CHANNELS = ['FZ', 'F7', 'F3', 'F4', 'F8']
+    POWER_COEFFICIENTS = ['Low', 'Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']
+    LSTM_SAMPLE_SIZE = [120]
+    WINDOW_SIZE = [10]
+
+    transferModule = TransferLearningModule()
+    utilsModule = Utils()
+
+    #Write experiment parameters
+    params = {'users':USERS,'eeg channels':EEG_CHANNELS,
+              'power coefficients':POWER_COEFFICIENTS,
+              'lstm sample sizes':LSTM_SAMPLE_SIZE,
+              'window size': WINDOW_SIZE}
+
+    for lstmSampleSize,windowSize in itertools.product(LSTM_SAMPLE_SIZE,WINDOW_SIZE):
+
+        lstmSteps = int(lstmSampleSize/windowSize)
+        completeResults = []
+
+        #['ryan','juan', 'jackie','jhony']
+        for user in ['jackie']:
+            dataPath = Path('./../../data/DifferentWindowSizeData/{:02d}s/'.format(windowSize))
+            resultsPath = Path('./results/EegResults/results_transfer10/')  \
+                          / 'temp3/'/ 'window{:02d}s_sampleSize{:02d}s'.format(windowSize,lstmSampleSize)
+
+            if not resultsPath.exists():
+                print('create ', resultsPath)
+                Path.mkdir(resultsPath,parents=True)
+
+            results = transferModule.testMultiUserModels(lstmSteps, dataPath,resultsPath,
+                                                             user,USERS,eegChannels=EEG_CHANNELS,
+                                                             powerCoefficients=POWER_COEFFICIENTS)
